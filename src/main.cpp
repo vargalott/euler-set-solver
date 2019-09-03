3640985eacd801e808c34c52a633dc7a45f233b1
vargalott <156085832+vargalott@users.noreply.github.com>
2019-09-03 16:13:04 +0300

    Implemented basic structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+//#include <chrono>
+#include <cstdlib>
+#include <cstddef>
+#include <iostream>
+#include <set>
+
+
+/*
+*	Entry Point
+*/
+int __cdecl main(void)
+{
+	return 0;
+};
